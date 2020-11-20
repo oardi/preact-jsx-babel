cb32de8d0fdcf9f0ec5c71395f097541fc729b82
Ardian Shala <ardian.sh@outlook.de>
2020-11-20 14:16:45 +0100

    init

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Preact Typescript TSX Webpack Starter
+
+Preact Typescript Starter with TSX, SCSS and Webpack 5.
+
+## Usage
+* `npm install`
+* `npm start`
+
+## Build
+* `npm run build`
+* `npm run build:prod`
+
+## Included
+- preact X
+- bootstrap
+- webpack 5
+- typescript 4
---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = (env, argv) => {
+
+	return {
+		context: path.resolve(__dirname, './src'),
+
+		entry: { app: './index.jsx' },
+
+		output: {
+			filename: '[name].[fullhash].bundle.js',
+			chunkFilename: '[name].[fullhash].bundle.js',
+			path: path.resolve(__dirname, 'dist')
+		},
+
+		devServer: {
+			open: true,
+			hot: true
+		},
+
+		// devtool: "source-map",
+
+		resolve: {
+			extensions: ['.js', '.jsx']
+		},
+
+		module: {
+			rules: [
+				{
+					test: /\.jsx?$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader'
+				},
+				{
+					test: /\.s[ac]ss$/i,
+					use: [
+						MiniCssExtractPlugin.loader,
+						"css-loader",
+						"sass-loader"
+					]
+				},
+				{
+					test: /\.(png|jpg|gif)$/,
+					use: [
+						{
+							loader: 'file-loader',
+							options: {
+								name: '[name].[ext]',
+								outputPath: 'assets/img',
+								publicPath: 'assets/img'
+							}
+						}
+					]
+				}
+			],
+		},
+
+		plugins: [
+			new CleanWebpackPlugin(),
+			new HtmlWebpackPlugin({
+				template: "./index.html",
+				title: 'Preact Jsx Babel',
+				filename: "index.html",
+				chunksSortMode: "manual",
+				chunks: ['vendors', 'app'],
+				favicon: 'favicon.ico'
+			}),
+			new MiniCssExtractPlugin({
+				filename: "style.css",
+				chunkFilename: "style.css"
+			})
+		],
+
+		optimization: {
+			minimize: true,
+			minimizer: [new TerserPlugin()],
+			splitChunks: {
+				cacheGroups: {
+					commons: { test: /[\\/]node_modules[\\/]/, name: 'vendors', chunks: 'all' }
+				}
+			}
+		}
+	}
+}
